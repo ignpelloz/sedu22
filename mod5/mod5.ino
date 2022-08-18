a08916c877f2eaafbb5c2fa1fa5defdfd41c4831
ignpelloz <ipeluaga@gmail.com>
2022-08-18 23:38:32 +0200

    Mod5: seems to be ready

--- mod5/mod5.ino
+++ mod5/mod5.ino
@@ -7,7 +7,6 @@
 
 //Constants
 #define DHTTYPE DHT11   // Tengo DHT 11 en lugar de 22
-#define NUMSENSORES 5
 
 //Pines
 // Pines
@@ -25,7 +24,7 @@ int estado = 1;
 char charRecibido;
 char charInicio = '[';
 char charFin = ']';
-char delimitador = '/';
+//char delimitador = '/';
 int posblesActuadores[] = {0,1};
 int actuadorAAccionar;
 int numeroDeActuadores = 2;
@@ -64,8 +63,6 @@ void activarActuador( void *pvParameters );
 // Semaforos
 SemaphoreHandle_t semaforoLecturaSensores;  // Mutex semaforo para permitir la lectura de los sensores
 SemaphoreHandle_t semaforoActivacionActuador;  // Mutex semaforo para permitir la activacion del actuador
-bool retakeSAA = true;
-bool retakeSLS = true;
 
 // Cola
 QueueHandle_t cola;
@@ -95,76 +92,6 @@ struct lecturaSensoresStruct {
     float imuy;
 };
 
-struct coordenadas getImuMeasurement(){
-  //Leer los valores del Acelerometro de la IMU
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B); //Pedir el registro 0x3B - corresponde al Acx
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU,6, true); //A partir del 0x3B, se piden 6 registros
-  AcX=Wire.read()<<8|Wire.read(); //Cada valor ocupa 2 registros
-  AcY=Wire.read()<<8|Wire.read();
-  AcZ=Wire.read()<<8|Wire.read();
-
-  //A partir de los valores del acelerometro, se calculan los angulos Y, X respectivamente, con la formula de la tangente.
-  Acc[1] = atan(-1*(AcX/A_R)/sqrt(pow((AcY/A_R),2) + pow((AcZ/A_R),2)))*RAD_TO_DEG;
-  Acc[0] = atan((AcY/A_R)/sqrt(pow((AcX/A_R),2) + pow((AcZ/A_R),2)))*RAD_TO_DEG;
-
-  //Leer los valores del Giroscopio
-  Wire.beginTransmission(MPU);
-  Wire.write(0x43);
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU,4,true); //A diferencia del Acelerometro, solo se piden 4 registro:
-  GyX=Wire.read()<<8|Wire.read();
-  GyY=Wire.read()<<8|Wire.read();
-
-  //Calculo del angulo del Giroscopio
-  Gy[0] = GyX/G_R;
-  Gy[1] = GyY/G_R;
-
-  //aplicar el Filtro Complenentario
-  struct coordenadas coordenadasIMU;
-  coordenadasIMU.x = 0.98 *(Angle[0]+Gy[0]*0.010) + 0.02*Acc[0];
-  coordenadasIMU.y = 0.98 *(Angle[1]+Gy[1]*0.010) + 0.02*Acc[1];
-  return coordenadasIMU;
-}
-
-int generarChecksum(float sensores[]){
-  float res = 0.0;
-  for (int i = 0; i < NUMSENSORES ; i++) { // realiza el sumatorio de los valores de los sensores
-    res += sensores[i];
-  }
-  String resString = (String)res; // Se convierte lo obtenido en una cadena
-  char resByte = resString[resString.indexOf('.')-1]; // Se toma el ultimo caracter a la izquierda del punto (ya que es un float): ese será el checksum
-  return resByte;
-}
-
-String consultarSensores(){
-  /* Devuelve un string del tipo [34/2,3/6,1/3] */
-
-  float sensores[NUMSENSORES];
-
-  // Intensidad de la luz (LDR)
-  sensores[0] = analogRead(ldrPin);
-
-  // Humedad
-  sensores[1] = dht.readHumidity();
-
-  // Temperatura
-  sensores[2] = dht.readTemperature();
-
-  // Acelerometro y Giroscopio (IMU)
-  struct coordenadas imuMeasurements = getImuMeasurement();
-  sensores[3] = imuMeasurements.x; // [0];
-  sensores[4] = imuMeasurements.y; // [1];
-
-  String trama = (String)charInicio + (String)"O";
-
-  for (int i = 0; i < NUMSENSORES ; i++) {
-    trama = trama + (String)sensores[i] + delimitador;
-  }
-  return trama + (generarChecksum(sensores)-'0') + charFin;
-}
-
 void estadoError(){
   ledBlink(ledRojoPin);
   estado = 1;
@@ -178,15 +105,6 @@ boolean arrayContieneElemento(int array[], int arrayLen, int elemento) {
     }
   }
   return false;
-}
-
-void realizarMovimiento(){
-  /* Activa el actuador actuadorAAccionar en el nivel que indica movimientoSolicitado */
-  if (actuadorAAccionar == 0){
-    servoMotor.write(movimientoSolicitado*20); // va de 0 a 180
-  } else if (actuadorAAccionar == 1){
-    analogWrite(INA, movimientoSolicitado*28); // va de 0 a 255
-  }
 }
 
 void ledBlink(int ledPin){
@@ -236,73 +154,7 @@ void setup() {
 
 }
 
-void loop() {}
-
-void loop_og_mod3 () { // TODO: this has to be moved to recibirPorPuertoSerie, where I do 'Serial.available() <= 0'
-  if (Serial.available() > 0){
-    charRecibido = Serial.read();
-    switch (estado) {
-      case 1: {
-        if (charRecibido == '['){
-          estado = 2;
-        }
-        break;
-      }case 2: {
-        if (charRecibido == 'S' || charRecibido == 'A'){
-          estado = 3;
-        }else{
-          estadoError();
-        }
-        break;
-      }case 3: {
-        if (charRecibido == ']'){
-          ledBlink(ledVerdePin);
-          Serial.println(consultarSensores());
-          estado = 1;
-        }else if(charRecibido == ','){
-          estado = 4;
-        }else{
-          estadoError();
-        }
-        break;
-      }case 4: {
-        if (arrayContieneElemento(posblesActuadores, numeroDeActuadores, (charRecibido-'0'))){
-          estado = 5;
-          actuadorAAccionar = (charRecibido-'0');
-        }else{
-          estadoError();
-        }
-        break;
-      }case 5: {
-        if (charRecibido == ','){
-          estado = 6;
-        }else{
-          estadoError();
-        }
-        break;
-      }case 6: {
-        if (arrayContieneElemento(posiblesMovimientos, numeroDeMovimientos, (charRecibido-'0'))){
-          estado = 7;
-          movimientoSolicitado = (charRecibido-'0');
-        }else{
-          estadoError();
-        }
-        break;
-      }case 7: {
-        if (charRecibido == ']'){
-          ledBlink(ledAzulPin);
-          realizarMovimiento();
-        }else{
-          estadoError();
-        }
-        estado = 1;
-        break;
-      }
-      default:
-        break;
-    }
-  }
-}
+void loop() {} // mod5's og loop: https://github.com/ignpelloz/sedu22/blob/main/mod5/mod5_sinEj4.ino#L227
 
 // ############################ FUNCIONES AUX ############################
 
@@ -437,18 +289,68 @@ void recibirPorPuertoSerie(void *pvParameters){
   for (;;){
     Serial.println(F("------------------------------------------------------------------------"));
     Serial.println(F("En recibirPorPuertoSerie esperando recibir algo por el puerto serie..."));
-    while (Serial.available() <= 0){} // TODO: que pasa si recibo algo que no es 0/1 ? si no pasa nada, ignorar
-    cmd = Serial.read();
-    if (cmd == '1') {
-      Serial.println(F("Recibido 1"));
-      retakeSAA = true;
-      retakeSLS = false;
-      xSemaphoreGiveFromISR(semaforoActivacionActuador, &xHigherPriorityTaskWoken_sAA);
-    } else if (cmd == '0') {
-      Serial.println(F("Recibido 0"));
-      retakeSAA = false;
-      retakeSLS = true;
-      xSemaphoreGiveFromISR(semaforoLecturaSensores, &xHigherPriorityTaskWoken_sLS);
+    while (Serial.available() <= 0){} // Espera a recibir algo por el puerto serie
+    charRecibido = Serial.read();
+    switch (estado) {
+      case 1: {
+        if (charRecibido == '['){
+          estado = 2;
+        }
+        break;
+      }case 2: {
+        if (charRecibido == 'S' || charRecibido == 'A'){
+          estado = 3;
+        }else{
+          estadoError();
+        }
+        break;
+      }case 3: {
+        if (charRecibido == ']'){
+          ledBlink(ledVerdePin);
+          //Serial.println(consultarSensores());
+          xSemaphoreGiveFromISR(semaforoLecturaSensores, &xHigherPriorityTaskWoken_sLS);
+          estado = 1;
+        }else if(charRecibido == ','){
+          estado = 4;
+        }else{
+          estadoError();
+        }
+        break;
+      }case 4: {
+        if (arrayContieneElemento(posblesActuadores, numeroDeActuadores, (charRecibido-'0'))){
+          estado = 5;
+          actuadorAAccionar = (charRecibido-'0');
+        }else{
+          estadoError();
+        }
+        break;
+      }case 5: {
+        if (charRecibido == ','){
+          estado = 6;
+        }else{
+          estadoError();
+        }
+        break;
+      }case 6: {
+        if (arrayContieneElemento(posiblesMovimientos, numeroDeMovimientos, (charRecibido-'0'))){
+          estado = 7;
+          movimientoSolicitado = (charRecibido-'0');
+        }else{
+          estadoError();
+        }
+        break;
+      }case 7: {
+        if (charRecibido == ']'){
+          ledBlink(ledAzulPin);
+          xSemaphoreGiveFromISR(semaforoActivacionActuador, &xHigherPriorityTaskWoken_sAA);
+        }else{
+          estadoError();
+        }
+        estado = 1;
+        break;
+      }
+      default:
+        break;
     }
     vTaskDelay(1); // Delay de 1 tick (15ms) para estabilidad (TODO: sin este delay aqui, esta tarea toma el semaforo de nuevo antes que activarActuador
   }
@@ -459,11 +361,11 @@ void activarActuador(void *pvParameters){
   for (;;){
     if (xSemaphoreTake(semaforoActivacionActuador, 1000) == pdTRUE){ // espera semaforo
       Serial.println(F("En activarActuador"));
-      servoMotor.write(servoPos); // Mueve el servo a su posicion maxima
-      if (servoPos == 0 ){
-        servoPos = 180;
-      } else {
-        servoPos = 0;
+      /* Activa el actuador actuadorAAccionar en el nivel que indica movimientoSolicitado */
+      if (actuadorAAccionar == 0){
+        servoMotor.write(movimientoSolicitado*20); // va de 0 a 180
+      } else if (actuadorAAccionar == 1){
+        analogWrite(INA, movimientoSolicitado*28); // va de 0 a 255
       }
     }
     vTaskDelay(1);  // Delay de 1 tick (15ms) para estabilidad
